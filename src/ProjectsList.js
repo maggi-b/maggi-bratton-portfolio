101d2366a0c7c0d2afb0d49428abbd4e2134ed7c
maggi-bratton <maggi.meins@gmail.com>
2023-05-26 11:59:59 -0600

    Initial commit

---
+++ src/ProjectsList.js
@@ -0,0 +1,42 @@
+let projects = [
+    {
+        name: "GitHub Repo Gallery",
+        explanation: "",
+        image: "../img/github-repo-gallery-thumbnail.png",
+        skills: "JavaScript",
+        tools: "Rest APIs, GitHub Pages",
+        siteLink: "https://maggi-b.github.io/github-repo-gallery/"
+    },
+    {
+        name:"Unplugged",
+        explanation: "",
+        image: "../img/responsive-unplugged-thumbnail.png",
+        skills: "HTMl, CSS, Flexbox",
+        tools: "GitHub Pages, Chrome DevTools",
+        siteLink: "https://maggi-b.github.io/unplugged/"
+    },
+    {
+        name: "Super Sticky Notes",
+        explanation: "",
+        image: "../img/super-sticky-notes-thumbnail.png",
+        skills: "React, JSX, Components",
+        tools: "CodeSandBox, Props, State",
+        siteLink: "https://t3d05f.csb.app/"
+    },
+    {
+        name: "Rogue Pickings",
+        explanation: "",
+        image: "../img/responsive-rogue-pickings.png",
+        skills: "HTML, CSS, Flexbox",
+        tools: "GitHub Pages, Chrome DevTools",
+        siteLink: "https://maggi-b.github.io/responsive-rogue-pickings/#menu",
+    },
+    {
+        name: "Guess the Word Game",
+        explanation: "",
+        image: "../img/guess-the-word-thumbnail.png",
+        skills: "JavaScript",
+        tools: "GitHub Pages, JSON",
+        siteLink: "https://maggi-b.github.io/guess-the-word-game/",
+    }
+];
