6e1efe58cd49027962fd02aa5143412cf9f5d4e9
maggi-bratton <maggi.meins@gmail.com>
2023-05-27 13:33:02 -0600

    Styled the projects section

--- src/projects/RenderProjectsArray.js
+++ src/projects/RenderProjectsArray.js
@@ -5,11 +5,11 @@ import '../App.css';
 const RenderProject = (project) => (
     <li className="projects-list" key={project.id}>
         <img src={project.image} alt={project.name}></img>
-        <p>{project.name}</p>
-        <p>{project.explanation}</p>
-        <p>Skills: {project.skills}</p>
-        <p>Tools: {project.tools}</p>
-        <a href={project.siteLink}target="_blank" className="images">Live Page</a>
+        <p className="project-name">{project.name}</p>
+        <p className="project-description">{project.explanation}</p>
+        <p className="skills-container">Skills: {project.skills}</p>
+        <p className="tools-container">Tools: {project.tools}</p>
+        <a href={project.siteLink}target="_blank" rel="noreferrer" className="site-link">Live Page</a>
     </li>
 );
 
