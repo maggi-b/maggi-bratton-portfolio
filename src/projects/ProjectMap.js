5055bda227e63ad0a6d142a5f7ea62db0383945b
maggi-bratton <maggi.meins@gmail.com>
2023-06-01 18:22:42 -0600

    Fixed project section layout for tablets

--- src/projects/ProjectMap.js
+++ src/projects/ProjectMap.js
@@ -2,6 +2,6 @@ import RenderProject from "./RenderProjectsArray";
 
 const ProjectMap = props => {
     const projectProperties = props.projects.map(RenderProject);
-    return <ul className="projects--unordered-list">{projectProperties}</ul>
+    return <ul className="projects-unordered-list">{projectProperties}</ul>
 };
 export default ProjectMap;
