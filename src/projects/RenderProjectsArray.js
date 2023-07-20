b6c651eb5a1e5d1236d237212919a5ebbfd398b6
maggi-bratton <maggi.meins@gmail.com>
2023-07-20 14:44:36 -0600

    Added links to images, made the scroll smooth, worked on max width

--- src/projects/RenderProjectsArray.js
+++ src/projects/RenderProjectsArray.js
@@ -4,8 +4,7 @@ import '../App.css';
 
 const RenderProject = (project) => (
     <li className="project-list-item" key={project.id}>
-        <img src={project.image} alt={project.name}></img>
-        <p className="project-name">{project.name}</p>
+        <a href={project.siteLink}target="_blank" rel="noreferrer"><img src={project.image} alt={project.name}></img></a>
         <p className="project-description">{project.explanation}</p>
         <p className="skills-container"><span>Skills:</span> {project.skills}</p>
         <p className="tools-container"><span>Tools:</span> {project.tools}</p>
